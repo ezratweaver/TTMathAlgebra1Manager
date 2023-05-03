0dba8f54ca154afc65880907384349b08a22ea67
ezratweaver <ezratweaver@gmail.com>
2023-05-03 17:20:25 -0500

    Merged files

--- database_manger.py
+++ database_manger.py
@@ -79,3 +79,15 @@ def sift_lesson_data(input_data, max_lessons):
     progress_percentage = f"{get_average(total_lessons, max_lessons)}%"
     grade_percentage = f"{get_average(total_percentage, total_lessons)}%"
     return progress, progress_percentage, grade_percentage
+
+DATABASE_NAME = "algebra1dat"
+PUBLIC_FOLDER = "TT Algebra 1"
+TOTAL_LESSONS = 142
+
+lesson_data = grab_lesson_data(
+    PUBLIC_FOLDER, DATABASE_NAME)
+progress, progress_percentage, grade_percentage = sift_lesson_data(
+    lesson_data, TOTAL_LESSONS)
+
+print(f"Lessons: {progress} Complete: {progress_percentage}")
+print(f"Total Grade: {grade_percentage}")
