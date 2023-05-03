28e3540f158865d6839a7f96370b2f1b8b5fc58e
ezratweaver <ezratweaver@gmail.com>
2023-05-03 10:55:27 -0500

    Changed Formatting | Added Button

--- gui_main.py
+++ gui_main.py
@@ -14,43 +14,41 @@ class MainScreen():
         )
 
         self.background = self.main_screen_canvas.create_image(
-            192,
-            125,
-            image=Assets.image_background
-        )
+            192, 125, image=Assets.image_background)
 
         self.main_titlebanner = self.main_screen_canvas.create_image(
-            192,
-            40,
-            image=Assets.image_titlebanner
-        )
+            192, 40, image=Assets.image_titlebanner)
 
         self.main_title = self.main_screen_canvas.create_image(
-            195,
-            38,
-            image=Assets.image_main_title
-        )
+            195, 38, image=Assets.image_main_title)
 
         self.progress_button_bg = self.main_screen_canvas.create_image(
-            192,
-            115,
-            image=Assets.image_button_large
-        )
+            192, 115, image=Assets.image_button_large)
 
         self.grade_button_bg = self.main_screen_canvas.create_image(
-            192,
-            165,
-            image=Assets.image_button_large
-        )
+            192, 165, image=Assets.image_button_large)
 
         self.volume_button_bg = self.main_screen_canvas.create_image(
-            192,
-            215,
-            image=Assets.image_button_large
+            192, 215, image=Assets.image_button_large)
+        
+        self.progress_button = Button(
+            image=Assets.image_progress,
+            borderwidth= 0,
+            bg="#F58025",
+            activebackground="#F58025"
         )
 
+        self.progress_button.place(
+            x=158, y=103,
+            width= 73, height= 22
+        )
+
+        self.grade_button = Button(
 
 
+
+            
+        )
 
     def show_canvas(self):
         self.main_screen_canvas.pack()
