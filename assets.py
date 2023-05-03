676e49e467caef96ee2cd6d8a6f1b57657f2e887
ezratweaver <ezratweaver@gmail.com>
2023-05-03 10:37:43 -0500

    Moved gui files

---
+++ assets.py
@@ -0,0 +1,21 @@
+from tkinter import Tk, PhotoImage
+from sys import argv
+from os import path, chdir
+
+EXE_DIR = path.dirname(argv[0])
+ASSETS_PATH = path.join(EXE_DIR, 'assets')
+chdir(EXE_DIR)
+
+window = Tk()
+
+window.geometry("384x251")
+window.resizable(False, False)
+
+class Assets:
+    image_background = PhotoImage(file="assets/background.png")
+    image_titlebanner = PhotoImage(file='assets/titlebanner.png')
+    image_button_large = PhotoImage(file='assets/button_large.png')
+    image_main_title = PhotoImage(file='assets/title.png')
+    image_grade = PhotoImage(file='assets/grade.png')
+    image_progress = PhotoImage(file='assets/progress.png')
+    image_volume = PhotoImage(file='assets/volume.png')
