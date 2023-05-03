25313efacdc028bd5fd4c1c9884c693e4b9b4e24
ezratweaver <ezratweaver@gmail.com>
2023-05-02 20:55:17 -0500

    Added documentation to functions

--- database_manger.py
+++ database_manger.py
@@ -3,6 +3,22 @@ from os import path, environ, chdir
 from get_average import get_average
 
 def grab_lesson_data(public_folder, database):
+    """
+    Goes into the Teaching Textbooks local database and collects data from the
+    LNum (Lesson Number) and LScore (Lesson Score) columns and returns a large list with
+    tuples containing the data
+
+    Args:
+        public_folder (string): The name of the folder of the TT Math database
+                                this is a agrument to possibly support other legacy
+                                TT math versions
+        database (string): The name of the database file the program will dig through
+                            agrument for the same reason as previous
+    Returns:
+        A large list of tuples, the tuples contain the lesson number and lesson score
+        stored as digits such as '1 | 1 | 0 | 1' to represent correct and wrong scores
+        1's represent correct and 0's represent incorrect
+    """
     tt_math_dir = path.join(environ["PUBLIC"], "Documents", public_folder)
     chdir(tt_math_dir)
     database_connection = connect(database)
@@ -13,10 +29,35 @@ def grab_lesson_data(public_folder, database):
     database_connection.close()
     return lesson_and_score
 
-def sift_lesson_data(input_db, max_lessons):
+def sift_lesson_data(input_data, max_lessons):
+    """
+    Sifts through a predetermined style of data and comes up with statistical data
+    for a students math performance
+    
+    Args:
+        input_data (list): A large list of tuples, where the tuples contain the lesson
+                            number and lesson score stored as digits in the style
+                            as such '1 | 0 | 1 | 1...' for the algorithm the count
+                            through
+        max_lessons (int): A constant that contains the curriculums max amount of
+                            lessons for proper tracking of progress
+    Returns:
+        Three variables: progress, progress_percentage, grade_percentage 
+        
+        progress (string): A string showing the completed lessons, and the amount max
+                            amount of lessons in the cirriculum EX. "23/142"
+        progress_percentage (string): A string percentage of the cirriculum
+                            completed, found by dividing the amount of lessons done by
+                            the max amount of lessons
+        grade_percentage (string): A string percentage of the total grade accuracy of
+                            the student, found by dividing the correct score with the
+                            incorrect and correct score combined all added into one
+                            value, then divided by the complete amount of lessons
+
+    """
     total_lessons = 0
     total_percentage = 0
-    for lesson in input_db:
+    for lesson in input_data:
         split_lesson_data = [int(x) for x in lesson[1].split(" | ")[10:]]
         problem_correct = 0
         problem_wrong = 0
