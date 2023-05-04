1255b53485442c4d708428ef467fb910d4a8f04d
ezratweaver <ezratweaver@gmail.com>
2023-05-03 22:15:48 -0500

    Added Progress Screen Controls

--- controller.py
+++ controller.py
@@ -1,30 +1,31 @@
 from gui_main import MainScreen, window
-from gui_progress import *
+from gui_progress import ProgressScreen
 from gui_grade import *
 from gui_volume import *
 
 main_screen = MainScreen()
+progress_screen = ProgressScreen()
 
 def progress_button_check():
     if main_screen.progress_button_pressed:
         main_screen.progress_button_pressed = False
         main_screen.hide_canvas()
-        # progress_screen.show_canvas()
-    window.after(70, progress_button_check)
+        progress_screen.show_canvas()
+    window.after(50, progress_button_check)
 
 def grade_button_check():
     if main_screen.grade_button_pressed:
         main_screen.grade_button_pressed = False
         main_screen.hide_canvas()
         # grade_screen.show_canvas()
-    window.after(70, grade_button_check)
+    window.after(50, grade_button_check)
     
 def volume_button_check():
     if main_screen.volume_button_pressed:
         main_screen.volume_button_pressed = False
         main_screen.hide_canvas()
         # volume_screen.show_canvas()
-    window.after(70, volume_button_check)
+    window.after(50, volume_button_check)
 
 progress_button_check()
 grade_button_check()
