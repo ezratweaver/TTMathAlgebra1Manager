841ad082105e29acfc76ab0f90886431f7cfbc44
ezratweaver <ezratweaver@gmail.com>
2023-05-03 21:07:06 -0500

    Added more assets

--- assets.py
+++ assets.py
@@ -15,7 +15,9 @@ class Assets:
     image_background = PhotoImage(file="assets/background.png")
     image_titlebanner = PhotoImage(file='assets/titlebanner.png')
     image_button_large = PhotoImage(file='assets/button_large.png')
+    image_button_small = PhotoImage(file="assets/button_small.png")
     image_main_title = PhotoImage(file='assets/title.png')
     image_grade = PhotoImage(file='assets/grade.png')
     image_progress = PhotoImage(file='assets/progress.png')
     image_volume = PhotoImage(file='assets/volume.png')
+
