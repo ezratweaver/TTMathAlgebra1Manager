e123dcba9af61821b0efb881a03344e757ef888b
ezratweaver <ezratweaver@gmail.com>
2023-05-04 20:16:00 -0500

    New interface file | Added Variable

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from controller import main_screen, window
+from database_manger import grab_lesson_data, sift_lesson_data
 
-if __name__ == "__main__":
-    main_screen.show_canvas()
-    window.mainloop()
+DATABASE_NAME = "algebra1dat"
+PUBLIC_FOLDER = "TT Algebra 1"
+TOTAL_LESSONS = 142
+
+lesson_data = grab_lesson_data(
+    PUBLIC_FOLDER, DATABASE_NAME)
+progress, progress_percentage, grade_percentage, grade = sift_lesson_data(
+    lesson_data, TOTAL_LESSONS)
+
+print(f"Lessons: {progress} ({progress_percentage})")
+print(f"Total Grade: {grade_percentage} ({grade})")
