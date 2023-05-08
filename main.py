0a73969ce2cf2b1d01d773d033c85db3beb614d5
ezratweaver <ezratweaver@gmail.com>
2023-05-08 10:37:45 -0500

    More consice coding

--- main.py
+++ main.py
@@ -12,16 +12,11 @@ TOTAL_TESTS = 19
 lesson_data = grab_math_data(PUBLIC_FOLDER, DATABASE_NAME)
 test_data = grab_math_data(PUBLIC_FOLDER, DATABASE_NAME, test=True)
 
-lnumbers, lprogress_percentage, lgrade_letter, lgrade_percentage = sift_math_data(
-    lesson_data, TOTAL_LESSONS)
-
-tnumbers, tprogress_percentage, tgrade_letter, tgrade_percentage = sift_math_data(
-    test_data, TOTAL_TESTS)
-
+lesson_variables = sift_math_data(lesson_data, TOTAL_LESSONS)
+test_variables = sift_math_data(test_data, TOTAL_TESTS)
 
 print("Welcome to Teaching Textbooks Algebra 1 Manager\n")
 print("[1] Lessons\n[2] Tests\n")
-
 try:
     user_view_choice = input("What would you like to view? ")
 except KeyboardInterrupt:
@@ -31,9 +26,9 @@ except KeyboardInterrupt:
 system("cls")
 
 if user_view_choice == "1":
-    print_gui("Lesson", lnumbers, lprogress_percentage, lgrade_letter, lgrade_percentage)
+    print_gui("Lesson", *lesson_variables)
 elif user_view_choice == "2":
-    print_gui("Test", tnumbers, tprogress_percentage, tgrade_letter, tgrade_percentage)
+    print_gui("Test", *test_variables)
 else:
     system('cls')
     print("Invalid Input")
