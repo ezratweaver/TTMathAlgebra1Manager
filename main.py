5de74f6ba40c2ef6932e25aa6720e84d8ce03c10
ezratweaver <ezratweaver@gmail.com>
2023-05-08 09:43:29 -0500

    Made terminal GUI more versatile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from os import system
 from database_manger import grab_math_data, sift_math_data
-from terminal_gui import create_progress_bar, print_gui
+from terminal_gui import print_gui
 
 system("cls")
 
@@ -13,11 +13,8 @@ lesson_data = grab_math_data(
 lesson_numbers, progress_percentage, grade_percentage, grade_letter = sift_math_data(
     lesson_data, TOTAL_LESSONS)
 
-lesson_progress_bar = create_progress_bar(progress_percentage)
-grade_progress_bar = create_progress_bar(grade_percentage)
 
-
-print_gui(lesson_numbers, lesson_progress_bar, progress_percentage,
-          grade_letter, grade_progress_bar, grade_percentage)
+print_gui(lesson_numbers, progress_percentage,
+          grade_letter, grade_percentage)
 
 
